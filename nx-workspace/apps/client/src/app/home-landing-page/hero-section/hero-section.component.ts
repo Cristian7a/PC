9b47e05f0a80c568e03b9daccfa8986191b6a61e
giio7a <cristiangeovanycuautle@gmail.com>
2026-03-11 00:32:13 -0600

    Refactor services section animations and improve dialog functionality

--- nx-workspace/apps/client/src/app/home-landing-page/hero-section/hero-section.component.ts
+++ nx-workspace/apps/client/src/app/home-landing-page/hero-section/hero-section.component.ts
@@ -1,32 +1,15 @@
 import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { Button } from 'primeng/button';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [TranslatePipe, Button],
+  imports: [TranslatePipe, Button, AnimateOnScrollModule],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeroSectionComponent {
   private readonly translateService = inject(TranslateService);
-
-  heroCards = [
-    {
-      icon: 'pi pi-trophy',
-      title: this.translateService.instant('home.service.title'),
-      description: this.translateService.instant('home.service.description'),
-    },
-    {
-      icon: 'pi pi-sparkles',
-      title: this.translateService.instant('home.experience.title'),
-      description: this.translateService.instant('home.experience.description'),
-    },
-    {
-      icon: 'pi pi-clock',
-      title: this.translateService.instant('home.avaliable.title'),
-      description: this.translateService.instant('home.avaliable.description'),
-    },
-  ];
 }
